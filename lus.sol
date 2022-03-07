34de641d48985418f638eeec4d31f29240914e2f
pawan <pawanmeenazee@gmail.com>
2022-03-07 23:13:49 +0530

    check

---
+++ lus.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL--3.0
+pragma solidity >= 0.5<0.9.0;
+contract Array {
+uint[3] public arr;
+uint public count;
+function loop() public {
+// while(count<arr.length){
+// arr[count]=count;
+// count++;
+// }
+// function loop() public {
+// for(uint i=count;i<arr.length;i++){
+//  arr[count]=count;
+//  count++;
+// }
+// }
+do{
+ arr[count]=count;
+ count++;
+}while(count<arr.length);
+}
+}
