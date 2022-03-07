007cae8a9c5bf9fe9f7cb10fe9aab259ff34ffc6
pawan <pawanmeenazee@gmail.com>
2022-03-07 20:20:48 +0530

    check

---
+++ dary.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL--3.0
+pragma solidity >= 0.5<0.9.0;
+contract Array {
+ uint[] public arr;//Dynamic array in solidity
+function pushElement(uint item) public{
+arr.push(item);
+}
+function length() public view returns(uint){
+    return arr.length;
+}
+function popElement() public{
+    arr.pop();
+}
+}
