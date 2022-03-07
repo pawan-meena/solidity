b2ee580e24baea37dd45f586ec28a02855ea04d8
pawan <pawanmeenazee@gmail.com>
2022-03-07 19:40:22 +0530

    check

--- integers.sol
+++ integers.sol
@@ -4,3 +4,9 @@ contract local {
  int8 count=127; //large error
  int16 count=128; //fine
 }
+// pragma solidity >= 0.5<0.9.0;
+// contract local
+//  {
+//  uint8 count=255; //not come
+//  uint8 count=-2; //-ve value error
+// }
