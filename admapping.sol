562334da1847e21de207fa326b3f0efe8de0b7a1
pawan <pawanmeenazee@gmail.com>
2022-03-08 23:32:50 +0530

    check

---
+++ admapping.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL--3.0
+pragma solidity >= 0.5<0.9.0;
+contract local 
+{
+struct student{
+  string name;
+  uint class;
+}
+mapping(uint=>student) public data;
+
+function setter(uint _roll, string memory _name, uint _class) public {
+ data[_roll] = student(_name,_class);
+}
+}
