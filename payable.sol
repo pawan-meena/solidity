6bdfc2f1200164c9a446ab39a04348df4ebdc670
pawan <pawanmeenazee@gmail.com>
2022-03-09 18:46:47 +0530

    check

---
+++ payable.sol
@@ -0,0 +1,25 @@
+// SPDX-L// SPDX-License-Identifier: GPL--3.0
+pragma solidity >= 0.5<0.9.0;
+contract pay 
+{
+function payEther() public payable
+{
+
+}
+function getBlalance() public view returns(uint)
+{
+return address(this).balance;
+}
+}icense-Identifier: GPL--3.0
+pragma solidity >= 0.5<0.9.0;
+contract pay 
+{
+function payEther() public payable
+{
+
+}
+function getBlalance() public view returns(uint)
+{
+return address(this).balance;
+}
+}
